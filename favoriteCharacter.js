271f976639573fc69db9e5b4c3a4fd8b6999f569
nikhilpounikar <nikhilpounikar@gmail.com>
2023-07-15 12:45:57 +0530

    Favorite pages changes

--- favoriteCharacter.js
+++ favoriteCharacter.js
@@ -1,81 +1,113 @@
-$('#mainContainer').hide();
-{
-  
-  $(document).ready(function () {
-   
-    let favoriteCharacters = localStorage.getItem('favoriteCharacters');
-    // Parse the existing data as JSON (if any)
-    let parsedData = favoriteCharacters ? JSON.parse(favoriteCharacters) : [];
-    manipulatedDOMForCharacters(parsedData);
-  })
+let favoriteCharacters = localStorage.getItem('favoriteCharacters');
+// Parse the existing data as JSON (if any)
+let parsedData = favoriteCharacters ? JSON.parse(favoriteCharacters) : [];
+manipulatedDOMForCharactersNew(parsedData);
 
-  function manipulatedDOMForCharacters(charactersArray) {
+function manipulatedDOMForCharactersNew(charactersArray) {
 
-    // getting the card html from document for adding characters card dynamically
-    let card = document.getElementsByClassName('card')[0];
+  // getting the card html from document for adding characters card dynamically
+  let mainContainer = document.getElementById('mainContainer');
 
-    //iterate over character array
-    for (let character of charactersArray) {
+  //iterate over character array
+  for (let character of charactersArray) {
 
 
-     
-      //clone the dummy card along with its childs
-      let currentCard = card.cloneNode(true); // Clone the card element
+    // Create a card element
+    const card = document.createElement('div');
+    card.className = 'card';
 
-      // prepare the image src
-      let url = character.thumbnail.path + '.' + character.thumbnail.extension;
-      let anchor = currentCard.querySelector('.card-image a');
-      anchor.setAttribute('href','/superhero.html?characterId='+character.id);
-      // lets add the image source to image tag
-      let characterImg = currentCard.querySelector('.card-image img'); // Target the img tag
-      characterImg.src = url; // Set the src attribute of the img tag
-      
-      // setting id as characterid in order to fetch more details about purticula character
-      characterImg.setAttribute('id',character.id);
-      
-      let characterNameSpan = currentCard.querySelector('.card-info span');
+    // Create card image element
+    const cardImage = document.createElement('div');
+    cardImage.className = 'card-image';
 
-      characterNameSpan.innerHTML  = character.name;
+    // Create anchor element for the image
+    const imageLink = document.createElement('a');
+    imageLink.href = '/superhero.html?characterId=' + character.id;
 
-      let favCharacterRemoveButton = currentCard.querySelector('.card-info button');
-      
-      favCharacterRemoveButton.setAttribute('id', character.id);
-      favCharacterRemoveButton.addEventListener('click',function(){
-        
-        removeFromLocalStorage(character.id);
-      })
+    // Create image element
+    const image = document.createElement('img');
+    image.id = character.id;
+    image.src = character.thumbnail.path + '.' + character.thumbnail.extension;;
+    image.alt = '';
 
-           //console.log(characterNameSpan);
-      // Append the modified cloned card to the image-container
-      $('#mainContainer').append(currentCard);
-    }
+    // Append the image to the anchor element
+    imageLink.appendChild(image);
 
-    // hide the dummy card 
-    card.style.display = 'none';
-    $('#mainContainer').show();
+    // Append the anchor element to the card image
+    cardImage.appendChild(imageLink);
+
+    // Create card info element
+    const cardInfo = document.createElement('div');
+    cardInfo.className = 'card-info';
+
+    // Create character name element
+    const characterName = document.createElement('div');
+    characterName.className = 'character-name';
+
+    // Create span element for the character name
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = character.name;
+    // Append the span to the character name element
+    characterName.appendChild(nameSpan);
+
+    // Create remove from favorite element
+    const removeFromFavorite = document.createElement('div');
+    removeFromFavorite.className = 'add-to-favourite';
+    removeFromFavorite.setAttribute('data-tooltip', 'Remove');
+
+    // Create button element for removing from favorite
+    const favoriteRemoveButton = document.createElement('button');
+    favoriteRemoveButton.id = character.id;
+    favoriteRemoveButton.className = 'fav-character-btn';
+    favoriteRemoveButton.addEventListener('click', function () {
+      removeFromLocalStorage(character.id);
+    });
+
+    // Create heart icon element
+    const removeIcon = document.createElement('span');
+    removeIcon.textContent = 'X';
+    //heartIcon.className = 'fas fa-heart';
+
+    // Append the heart icon to the favorite button
+    favoriteRemoveButton.appendChild(removeIcon);
+
+    // Append the favorite button to the add to favorite element
+    removeFromFavorite.appendChild(favoriteRemoveButton);
+
+    // Append the character name and add to favorite elements to the card info
+    cardInfo.appendChild(characterName);
+    cardInfo.appendChild(removeFromFavorite);
+
+    // Append the card image and card info to the card
+    card.appendChild(cardImage);
+    card.appendChild(cardInfo);
+    mainContainer.appendChild(card);
+
+
   }
 
-  function removeFromLocalStorage(characterId) {
-    
-    // Retrieve existing data from local storage
-    let existingData = localStorage.getItem('favoriteCharacters');
-  
-    // Parse the existing data as JSON
-    let parsedData = existingData ? JSON.parse(existingData) : [];
-  
-    // Find the index of the character with the specified ID
-    const index = parsedData.findIndex(character => character.id === characterId);
-    console.log(index);
-    // If the character is found, remove it from the array
-    if (index !== -1) {
-      parsedData.splice(index, 1);
-  
-      // Update the data in local storage
-      localStorage.setItem('favoriteCharacters', JSON.stringify(parsedData));
-    }
-
-    location.reload();
-  }
-  
-  
 }
+
+function removeFromLocalStorage(characterId) {
+
+  // Retrieve existing data from local storage
+  let existingData = localStorage.getItem('favoriteCharacters');
+
+  // Parse the existing data as JSON
+  let parsedData = existingData ? JSON.parse(existingData) : [];
+
+  // Find the index of the character with the specified ID
+  const index = parsedData.findIndex(character => character.id === characterId);
+  console.log(index);
+  // If the character is found, remove it from the array
+  if (index !== -1) {
+    parsedData.splice(index, 1);
+
+    // Update the data in local storage
+    localStorage.setItem('favoriteCharacters', JSON.stringify(parsedData));
+  }
+
+  location.reload();
+}
+
+
